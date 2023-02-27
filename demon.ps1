c65c5fbfca4963d4a658b5ba79369cd076fb45f7
Mira Paulik <mira@cmgx.io>
2023-02-27 14:12:10 +0100

    feat: windows compatibility #1

--- demon.ps1
+++ demon.ps1
@@ -1,12 +1,13 @@
 . "$PSScriptRoot\src\utils.ps1"
 . "$PSScriptRoot\src\ares.ps1"
 
+$config = Parse-IniFile("$PSScriptRoot\examples\config.ini")
 $job = Parse-IniFile("$PSScriptRoot\examples\job.ini")
 
 switch ($job.uloha.typ)
 {
     "ares" {
-        DAres($job.parametry)
+        DAres($job.parametry, $config)
     }
 }
 
